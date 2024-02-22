87fa66a779cb289ff12308bb861058514bb49e1d
balakumarr25 <123769815+balakumarr25@users.noreply.github.com>
2024-02-22 15:24:02 +0530

    Add files via upload

---
+++ fashion-child-product.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>child clothes</title>
+    <link rel="stylesheet" href="style11.css" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+  </head>
+  <body>
+      <div class="container">
+        <div class="navbar">
+          <div class="logo">
+            <a href="index.html"> online shopping website</a>
+          </div>
+          <nav>
+            <ul id="MenuItems">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="product.html">product</a></li>
+                
+
+                
+               <li><a href="about.html">About</a></li>
+              <li><a href="contact.html">Contact</a></li>
+              <li><a href="account.html">Account</a></li>
+            </ul>
+          </nav>
+          
+        </div>
+      </div>
+<div class="small-container">
+   <div class="row row-2">
+    <h2>All Product</h2>
+    
+</div>
+  <div class="row">
+    <div class="col-4">
+      <img src="images/clothes/child clothes/cloth1.jpg" alt="">
+      <h4>Lancha</h4>
+      <p>500 rs.</p>
+    </div>
+    <div class="col-4">
+      <img src="images/clothes/child clothes/cloth2.jpg" alt="">
+      <h4>Lehnga</h4>
+      <p>500 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth3.jpg" alt="">
+        <h4>Frog </h4>
+      <p>200 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth4.jpg" alt="">
+        <h4>Skirt and Top </h4>
+      <p>250 rs.</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <img src="images/clothes/child clothes/cloth5.jpg" alt="">
+      <h4>Skirt and Top</h4>
+      <p>250 rs.</p>
+    </div>
+    <div class="col-4">
+      <img src="images/clothes/child clothes/cloth6.jpg" alt="">
+      <h4>Skirt and Top</h4>
+      <p>250 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth7.jpg" alt="">
+        <h4>Frog</h4>
+      <p>250 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth8.jpg" alt="">
+        <h4>Lancha</h4>
+      <p>550 rs.</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth9.jpg" alt="">
+        <h4>Skirt and Top</h4>
+      <p>200 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth10.jpg" alt="">
+        <h4>Capri</h4>
+      <p>250 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth11.jpg" alt="">
+        <h4>Legi Kurta</h4>
+      <p>300 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth12.jpg" alt="">
+        <h4>Capri & Top</h4>
+      <p>250 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth13.jpg" alt="">
+        <h4>Frog</h4>
+      <p>90 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth14.jpg" alt="">
+        <h4>Frog</h4>
+      <p>90 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth15.jpg" alt="">
+        <h4>Frog</h4>
+      <p>300 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth16.jpg" alt="">
+        <h4>Frog</h4>
+      <p>300 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth17.jpg" alt="">
+        <h4>Sharara</h4></a>
+      <p>450 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth18.jpg" alt="">
+        <h4>Pant Shirt</h4>
+      <p>350 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth19.jpg" alt="">
+        <h4>Pant Shirt</h4>
+      <p>350 rs.</p>
+    </div>
+    <div class="col-4">
+        <img src="images/clothes/child clothes/cloth3.jpg" alt="">
+        <h4>Frog</h4>
+      <p>250 rs.</p>    
+  </div>
+  </div>
+  
+  </div>
+      <!-- -------footer------ -->
+      <footer class="footer">
+        <div class="container-f">
+          <div class="row-f">
+            <div class="footer-col">
+              <h4>online shop</h4>
+              <ul>
+                
+                <li><a href="fashion-child-product.html">Fashion</a></li>
+                <li><a href="footwear-product.html">Footwear</a></li>
+                <li><a href="electronics-product.html">Electronics</a></li>
+                
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>get help</h4>
+              <ul>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">shipping</a></li>
+                <li><a href="#">returns</a></li>
+                <li><a href="#">order status</a></li>
+                <li><a href="#">payment options</a></li>
+              </ul>
+            </div> 
+              <div class="footer-col">
+                <h4>company</h4>
+                <ul>
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="contact.html">Contact Us</a></li>
+                  <li><a href="product.html">Our Services</a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                </ul>
+              </div>
+              <div class="footer-col">
+                <h4>follow us</h4>
+                <div class="social-links">
+                  
+                  <a href="https://www.instagram.com/balakumarr_25/" target="_blank"><i class="fab fa-instagram"></i></a>
+                  
+                </div>
+            </div>
+          </div>
+            <hr><div class="copyright text-center">
+              <h5><a href="#">🌐</a> copyright © BALAKUMAR all rights reserved</h5>
+              <br><small>♥ built by <a href="https://www.instagram.com/balakumarr_25/" target="_blank">BALAKUMAR
+
+
+
+
+              </a>-</small>
+            </div>
+          </div>  
+     </footer>
+    <!-- -------js for toggle menu----- -->
+    <script>
+      var MenuItems = document.getElementById("MenuItems");
+      var MenuItem = document.getElementById("inneritems");
+      MenuItems.style.maxHeight = "0px";
+    
+      function menutoggle(){
+        if(MenuItems.style.maxHeight == "0px")
+        {
+         MenuItems.style.maxHeight = "200px";
+        }
+        else
+        {
+         MenuItems.style.maxHeight = "0px";
+        }
+       
+      }
+      function menutoggl(){
+        if(MenuItem.style.display == "block")
+        {
+         MenuItem.style.display = "none";
+        }
+        else
+        {
+         MenuItem.style.display = "block";
+        }
+       
+      }
+         </script>
+  </body>
+</html>
---
+++ footwear-product.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>MY Shop</title>
+    <link rel="stylesheet" href="style.css" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  </head>
+  <body>
+    <div class="header">
+     <div class="container">
+          <div class="navbar">
+          <div class="logo">
+           <a href="index.html"> online shopping page </a>
+          </div>
+          <nav>
+            <ul id="MenuItems">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="product.html">product</a></li>
+                
+
+                
+               <li><a href="about.html">About</a></li>
+              <li><a href="contact.html">Contact</a></li>
+
+              <li><a href="http://localhost/Project1/register.php" target="_blank">Register</a></li>
+		<li><a href="http://localhost/Project1/login.php" target="_blank">login</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div class="row">
+          <div class="col-2">
+            <h1>
+              Here shopping becomes <br/>simple!
+            </h1>
+            <p>
+              "We trust something in a grocery store and assume it's good. We don't learn about the most precious thing in life—
+              <br/>the food we put in our body. Educate yourself!"
+            </p>
+            <a href="" class="btn">Explore Now &#10140;</a>
+          </div>
+          <div class="col-2">
+             <img src="images/image1.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  <!-- ---------featured categories--------   -->
+<div class="small-container">
+  <h2 class="title">Categories</h2>
+  <div class="row">
+    <div class="col-4">
+      <a href="fashion-child-product.html" target="_blank"><img src="images/clothes/category-3.png" alt=""></a>
+      <a href="fashion-child-product.html" target="_blank"><h4>Clothes</h4></a>
+      <a href="fashion-child-product.html" target="_blank"><p>explore more..</p></a>    
+    </div>
+    <div class="col-4">
+      <a href="gift-product.html"target="_blank"><img src="images/gifts/gift.png" alt=""></a>
+      <a href="gift-product.html"target="_blank"><h4>Gift</h4></a>
+      <a href="gift-product.html"target="_blank"><p>explore more..</p></a>
+      
+    </div>
+    <div class="col-4">
+      <a href="toys-product.html"target="_blank"><img src="images/Toys/toys.png" alt=""></a>
+      <a href="toys-product.html"target="_blank"><h4>Toys</h4></a>
+      <a href="toys-product.html"target="_blank"><p>explore more..</p></a>
+    </div>
+    <div class="col-4">
+      <a href="footwear-product.html"target="_blank"><img src="images/footware/foot.png" alt=""></a>
+      <a href="footwear-product.html"target="_blank"><h4>Footwear</h4></a>
+      <a href="footwear-product.html"target="_blank"><p>explore more..</p></a>
+    </div>
+  </div>
+  
+ 
+<!-- -----offer------ -->
+<div class="offer">
+<div class="small-container">
+  <div class="row">
+    <div class="col-2">
+<img src="images/electronic/speaker1.png" class="offer-img">
+    </div>
+    <div class="col-2">
+<p>Exclusively Available On SiyaShop</p>
+<h1>Wireless Portable Bluetooth Speakers</h1>
+<small>B M C Wireless Portable Bluetooth Speakers kt-125 with Mobile Holder, with USB, Memory Card and Bluetooth Connectivity (Kt-125)</small><br>
+<a href="product-detials-speaker.html" target="_blank" class="btn">Buy Now &#10140</a>
+    </div>
+  </div>
+</div>
+</div>
+
+      <!-- -------footer------ -->
+      <footer class="footer">
+        <div class="container-f">
+          <div class="row-f">
+            <div class="footer-col">
+              <h4>online shop</h4>
+              <ul>
+                
+                <li><a href="fashion-child-product.html">Fashion</a></li>
+                <li><a href="footwear-product.html">Footwear</a></li>
+                <li><a href="electronics-product.html">Electronics</a></li>
+                
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>get help</h4>
+              <ul>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">shipping</a></li>
+                <li><a href="#">returns</a></li>
+                <li><a href="#">order status</a></li>
+                <li><a href="#">payment options</a></li>
+              </ul>
+            </div> 
+              <div class="footer-col">
+                <h4>company</h4>
+                <ul>
+                  <li><a href="about.html">About Us</a></li>
+                  <li><a href="contact.html">Contact Us</a></li>
+                  <li><a href="product.html">Our Services</a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                </ul>
+              </div>
+              <div class="footer-col">
+                <h4>follow us</h4>
+                <div class="social-links">
+                  
+                  <a href="https://www.instagram.com/balakumarr_25/" target="_blank"><i class="fab fa-instagram"></i></a>
+                  
+                </div>
+            </div>
+          </div>
+            <hr><div class="copyright text-center">
+              <h5><a href="#">🌐</a> copyright © BALAKUMAR all rights reserved</h5>
+              <br><small>♥ built by <a href="https://www.instagram.com/balakumarr_25/" target="_blank">BALAKUMAR
+
+
+
+
+              </a>-</small>
+            </div>
+          </div>  
+     </footer>
+     <!-- -------js for toggle menu----- -->
+     <script>
+       var MenuItems = document.getElementById("MenuItems");
+       var MenuItem = document.getElementById("inneritems");
+       MenuItems.style.maxHeight = "0px";
+
+       function menutoggle(){
+         if(MenuItems.style.maxHeight == "0px")
+         {
+          MenuItems.style.maxHeight = "200px";
+         }
+         else
+         {
+          MenuItems.style.maxHeight = "0px";
+         }
+        
+       }
+       function menutoggl(){
+         if(MenuItem.style.display == "block")
+         {
+          MenuItem.style.display = "none";
+         }
+         else
+         {
+          MenuItem.style.display = "block";
+         }
+        
+       }
+     </script>
+  </body>
+</html>
